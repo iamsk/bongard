0c95d6be350839bac6a4522825095c2a51b224c6
iamsk <iamsk.info@gmail.com>
2014-10-03 11:29:26 +0800

    update author

--- server/test2gameInfo.py
+++ server/test2gameInfo.py
@@ -2,13 +2,27 @@ from json import load, dump
 
 BPs = load(open('test.json'))
 
+AUTHORS = {
+    'bongard': 'Mikhail M. Bongard',
+    'merse': 'Merse E. Gáspár',
+    'barenbaum': 'Pablo Barenbaum',
+    'ihde': 'Michael Ihde',
+    'gunnarsson': 'Andreas Gunnarsson',
+    'foundal': 'Harry E. Foundalis',
+    'insana': 'Joseph A. L. Insana',
+    'shanahan': 'Peter Shanahan',
+    'howells': 'Matthew J. Howells',
+    'doughof': 'Douglas R. Hofstadter',
+}
+
 
 def get_check_point(number, author):
     check_point = {
         "imagesPath": "./image/%s/" % number,
-        "author": author
+        "author": AUTHORS[author]
     }
     return check_point
+
 
 tpl = {
     "version": "0.1.0",
@@ -22,11 +36,16 @@ tpl = {
         }, {
             "name": "senior",
             "checkPoints": []
+        }, {
+            "name": "complex",
+            "checkPoints": []
         }
     ]
 }
 
 for bp in BPs:
+    if not bp['number'].isdigit():
+        continue
     cp = get_check_point(bp['number'], bp['author'])
     tpl['checkPointTypes'][0]['checkPoints'].append(cp)
 
