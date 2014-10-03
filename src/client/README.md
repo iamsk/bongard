06d724cf214df8c8a6e2a21689d1a9f9d89b9831
bwu-aa <bwu@appannie.com>
2014-10-03 21:05:31 +0800

    add admob

--- src/client/README.md
+++ src/client/README.md
@@ -3,5 +3,10 @@ How to build mobile on iOS?
     2. cd to mobile/bongard directory
     3. run "cordova platform add ios"
     4. add plugin for social sharing: "cordova plugin add https://github.com/EddyVerbruggen/SocialSharing-PhoneGap-Plugin.git"
-    5. run "cordova emulate ios" to run in simulator
-    6. run "cordova run ios" to run in connected devices.
+    5. add pluing for admob: "cordova plugin add com.rjfun.cordova.plugin.admob"
+    6. run "grunt build" to build all the required assets
+    7. run "rm -rf www" to remove the "www" folder
+    8. run "ln -sf ../../build www" to create a symbol link to the build assets
+    9. run "cordova prepare" for preparing
+    10. run "cordova emulate ios" to run in simulator
+    11. run "cordova run ios" to run in connected devices.
---
+++ src/client/www/js/admob.js
@@ -0,0 +1,53 @@
+ function onDeviceReady() {
+    initAd();
+
+    // display a banner at startup
+    window.plugins.AdMob.createBannerView();
+
+    // prepare the interstitial
+    window.plugins.AdMob.createInterstitialView();
+
+    // somewhere else, show the interstital, not needed if set autoShow = true
+    window.plugins.AdMob.showInterstitialAd();
+}
+function initAd(){
+    if ( window.plugins && window.plugins.AdMob ) {
+        var ad_units = {
+            ios : {
+                banner: 'ca-app-pub-8444566453872913/5347663750',
+                interstitial: 'ca-app-pub-8444566453872913/6824396950'
+            },
+            android : {
+                banner: 'ca-app-pub-8444566453872913/6545195354',
+                interstitial: 'ca-app-pub-8444566453872913/8021928550'
+            }
+        };
+        var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+
+        window.plugins.AdMob.setOptions( {
+            publisherId: admobid.banner,
+            interstitialAdId: admobid.interstitial,
+            bannerAtTop: false, // set to true, to put banner at top
+            overlap: false, // set to true, to allow banner overlap webview
+            offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+            isTesting: false, // receiving test ad
+            autoShow: true // auto show interstitial ad when loaded
+        });
+
+        registerAdEvents();
+
+    } else {
+        alert( 'admob plugin not ready' );
+    }
+}
+// optional, in case respond to events
+function registerAdEvents() {
+    document.addEventListener('onReceiveAd', function(){});
+    document.addEventListener('onFailedToReceiveAd', function(data){});
+    document.addEventListener('onPresentAd', function(){});
+    document.addEventListener('onDismissAd', function(){ });
+    document.addEventListener('onLeaveToAd', function(){ });
+    document.addEventListener('onReceiveInterstitialAd', function(){ });
+    document.addEventListener('onPresentInterstitialAd', function(){ });
+    document.addEventListener('onDismissInterstitialAd', function(){ });
+}
